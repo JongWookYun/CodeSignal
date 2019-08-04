36ab47cc0ee108b90f520f9c1b42c96351e4beed
JongWookYun <dbswhddnr456@naver.com>
2019-08-04 13:09:37 +0900

    test

---
+++ abcd.java
@@ -0,0 +1,14 @@
+import java.utli.Scanner;
+
+public class Level1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int num1,num2;
+
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+		
+		System.out.println("sum : " + (num1 + num2));
+	}
+}
